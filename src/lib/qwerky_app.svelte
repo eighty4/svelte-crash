3f105b4ead383e24385c05b6f1a8421a91cb072f
Adam McKee <adam.be.g84d@gmail.com>
2024-04-18 19:34:04 -0500

    trim extra files not needed to recreate issue

--- src/lib/qwerky_app.svelte
+++ src/lib/qwerky_app.svelte
@@ -1,40 +1,27 @@
 <script lang="ts">
     import {type Point, type Size} from '$lib/data/QwerkyMessages'
-    import UrlForm from '$lib/components/url_form.svelte'
     import {type AppData, createAppData} from '$lib/data/AppData.svelte'
-    import Footer from '$lib/footer/qwerky_footer.svelte'
-    import Header from '$lib/header/qwerky_header.svelte'
     import Panel from '$lib/panel/app_panel.svelte'
     import PageImage from '$lib/page/open_page.svelte'
     import img from '$lib/img.json'
     import inspects from '$lib/inspects.json'
 
     const appData: AppData = createAppData()
-    let pageLoading: boolean = $state(false)
-    let currentPageImage: { base64: string, size: Size } | null = $state(null)
-
-    function onUrl(event: CustomEvent<string>) {
-        appData.url = event.detail
-        currentPageImage = img
-    }
+    let currentPageImage: { base64: string, size: Size } | null = $state(img)
 
     function onInspectPoint(event: CustomEvent<Point>) {
         appData.addInspectResult(inspects[0])
     }
 </script>
 
-{#if !appData.url}
-    <UrlForm on:url={onUrl}/>
-{:else if currentPageImage !== null}
+{#if currentPageImage !== null}
     <PageImage boundingBoxes={appData.focusedInspection?.boundingBoxes}
                imageBase64={currentPageImage.base64}
                imageSize={currentPageImage.size}
                on:inspectPoint={onInspectPoint}/>
 {/if}
 
-<Header pageLoading={pageLoading} url={appData.url}/>
 <Panel appData={appData}/>
-<Footer/>
 
 <!-- todo offsetting stacked highlights to not overlap requires extending highlight buffer by highlight count -->
 <div class="hb t"></div>
